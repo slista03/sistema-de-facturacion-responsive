d204d406fc556c050cdd682a560e05600a6e9f82
LgsusGBP <jcubilete22@gmail.com>
2021-01-10 19:37:25 -0500

    m

--- conexion/php/conexion.php
+++ conexion/php/conexion.php
@@ -1,7 +1,7 @@
 <?php
     $host ='localhost';
     $user = 'root';
-    $password = 'jehova22';
+    $password = '';
     $db = 'sistema de facturacion';
 
     $conection = @mysqli_connect($host,$user,$password,$db);
--- sistema/eliminar_usuario.php
+++ sistema/eliminar_usuario.php
@@ -8,7 +8,6 @@
 			exit;
 		}
 		$idusuario=$_POST['idusuario'];
-		//$query_delete= mysqli_query($conection, "DELETE FROM usuario WHERE idusuario = $idusuario");
 		$query_delete = mysqli_query($conection,"UPDATE `usuario` SET `estatus` = '0' WHERE `usuario`.`idusuario` = $idusuario");
 		mysqli_close($conection);
 		if($query_delete){
